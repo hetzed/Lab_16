041640e3eab6c311a31471d738c023aac010d429
Felipe D Quinones <felipe.dj.quinones@gmail.com>
2017-12-09 19:44:27 -0700

    Setup class

---
+++ Class_CircleShape/SquareShape.cpp
@@ -0,0 +1,23 @@
+/*
+Author: Felipe D Quinones | 106148
+History:
+12/ 02 / 2017 - File created.[FQS]
+*/
+
+#include <iostream>
+using namespace std;
+
+class SquareShape {
+	float side;
+
+public:
+	void setSide(float);
+	float findArea();
+	float findPerimeter();
+
+};
+
+int main{
+
+}
+
